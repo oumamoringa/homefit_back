222e5ef27b856f584dc823492e190c48fadb8894
Richard Ouma <richard.ouma@student.moringaschool.com>
2022-12-06 15:07:37 +0300

    Adding review actions

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,4 +2,14 @@ class ReviewsController < ApplicationController
     def index
         render json: Review.all
         end
+
+        def create
+            review = Review.create(review_params)
+            if review.valid?
+                render json: {success:["Review added succesfully!"]}
+            else
+                render json: {errors: [review.errors.full_messages] }, status: :unprocessable_entity
+            end
+        end
+
 end
