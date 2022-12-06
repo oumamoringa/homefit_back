f2e71d4960398c89d84ab09fadae71d23b9d09ae
Richard Ouma <richard.ouma@student.moringaschool.com>
2022-12-06 14:18:15 +0300

    Addin username and email validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
     has_secure_password
+
+
+    # belongs_to :trainer
+    # has_many :tutorials ,through: :trainer
+    validates :email, presence: true, uniqueness: true
+    validates :username, presence: true, uniqueness: true
 end
